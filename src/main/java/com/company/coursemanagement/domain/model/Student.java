510d44f7262572e60d72847f1990246de6a15097
GenesisQ <suheidaquinonezgmail.com>
2026-08-15 13:51:37 -0500

    Conexión a MySQL y entidad Student

--- src/main/java/com/company/coursemanagement/domain/model/Student.java
+++ src/main/java/com/company/coursemanagement/domain/model/Student.java
@@ -1,16 +1,50 @@
 package com.company.coursemanagement.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(
+        name = "students" )
 
 public class Student {
+
+    @Id
+    @Column(name = "id"
+    )
     private Long id;
+
+    @NotBlank
+    @Column(
+            name = "first_name",
+            nullable = false,
+            length = 100
+    )
     private String firstName;
+
+    @Column(
+            name = "last_name",
+            nullable = false,
+            length = 100
+    )
     private String lastName;
+
+    @Column(
+            name = "email",
+            nullable = false,
+            unique = true,
+            length = 150
+    )
     private String email;
+
+    @Column(
+            name = "birth_date"
+    )
     private LocalDate birthDate;
 
-    public Student() {
-    }
+
 
     public Student(Long id, String firstName, String lastName, String email, LocalDate birthDate) {
         this.id = id;
@@ -20,18 +54,67 @@ public class Student {
         this.birthDate = birthDate;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getFirstName() {
+        return firstName;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-    public LocalDate getBirthDate() { return birthDate; }
-    public void setBirthDate(LocalDate birthDate) { this.birthDate = birthDate; }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
 }
